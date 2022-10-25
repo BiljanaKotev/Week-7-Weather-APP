9d965606482513a80cfb0dc6a924f51d59bb7b03
biljana_kotev <biljanakotev@gmail.com>
2022-10-25 14:23:58 +0200

    refactored my code

--- src/index.js
+++ src/index.js
@@ -38,22 +38,30 @@ function formatDate(timestamp) {
 function getWeather(response) {
   console.log(response);
   let tempDegree = document.querySelector("#degrees");
-  tempDegree.innerHTML = Math.round(response.data.temperature.current);
   let searchCity = document.querySelector("h2");
-  searchCity.innerHTML = response.data.city;
   let wind = document.querySelector("#wind-speed");
-  wind.innerHTML = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.temperature.humidity;
   let description = document.querySelector("#description");
-  description.innerHTML = response.data.condition.description;
   let dateTime = document.querySelector("#date-time");
-  dateTime.innerHTML = formatDate(response.data.time * 1000);
   let weatherIcon = document.querySelector("#weather-icon");
   weatherIcon.setAttribute(
     "src",
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
+
+  celsiusTemp = response.data.temperature.current;
+
+  tempDegree.innerHTML = Math.round(celsiusTemp);
+
+  searchCity.innerHTML = response.data.city;
+
+  wind.innerHTML = Math.round(response.data.wind.speed);
+
+  humidity.innerHTML = response.data.temperature.humidity;
+
+  description.innerHTML = response.data.condition.description;
+
+  dateTime.innerHTML = formatDate(response.data.time * 1000);
 }
 
 function search(city) {
@@ -69,29 +77,28 @@ function handleSubmit(event) {
   search(cityInputSearch.value);
 }
 
+let celsiusTemp = null;
+
 let form = document.querySelector("#form-submit");
 form.addEventListener("submit", handleSubmit);
 
-search("Athens");
-
-// Function for user to select celsius
-
-function changeDegreeUnitToCelsius() {
-  let degreeUnit = document.querySelector("#degrees");
-  let celsiusUnit = document.querySelector("#celsius");
-  if (celsiusUnit) {
-    degreeUnit.innerHTML = 15;
-  }
-}
-let celsiusClick = document.querySelector("#celsius");
-celsiusClick.addEventListener("click", changeDegreeUnitToCelsius);
-
 // Function for user to select fahrenheit
-function changeDegreeUnitToFahrenheit() {
-  let city = document.querySelector("h2");
-  let fahrenheitUnit = document.querySelector("#fahrenheit");
-  if (fahrenheitUnit) {
-  }
+function changeDegreeUnitToFahrenheit(event) {
+  event.preventDefault();
+  let degreeUnit = document.querySelector("#degrees");
+  let fahrenheitDegree = (celsiusTemp * 9) / 5 + 32;
+  degreeUnit.innerHTML = Math.round(fahrenheitDegree);
 }
 let fahrenheitClick = document.querySelector("#fahrenheit");
 fahrenheitClick.addEventListener("click", changeDegreeUnitToFahrenheit);
+
+function changeDegreeUnitToCelsius(event) {
+  event.preventDefault();
+  let degreeUnit = document.querySelector("#degrees");
+  degreeUnit.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusClick = document.querySelector("#celsius");
+celsiusClick.addEventListener("click", changeDegreeUnitToCelsius);
+
+search("Athens");
